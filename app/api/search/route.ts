7c6d80caa1c27c08cfbe07456e48f3904d1d7eff
N0ctane._.Dev <n0ctaneDev@gmail.com>
2026-04-22 07:42:22 +0530

    Update route.ts

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,11 +1,33 @@
-import { getPages } from "@/app/source";
-import { createSearchAPI } from "fumadocs-core/search/server";
+import { source } from '@/lib/source'
+import { createFromSource } from 'fumadocs-core/search/server'
+import { createTokenizer } from '@orama/tokenizers/mandarin'
 
-export const { GET } = createSearchAPI("advanced", {
-  indexes: getPages().map((page) => ({
-    title: page.data.title,
-    structuredData: page.data.exports.structuredData,
-    id: page.url,
-    url: page.url,
-  })),
-});
+// statically cached
+export const revalidate = false
+export const { staticGET: GET } = createFromSource(source, {
+  // https://docs.orama.com/docs/orama-js/supported-languages
+  // 不同语言的搜索配置
+  localeMap: {
+    // [locale]: Orama options
+    cn: {
+      components: {
+        tokenizer: createTokenizer(),
+      },
+      search: {
+        threshold: 0,
+        tolerance: 0,
+      },
+    },
+    en: { language: 'english' },
+  },
+  buildIndex(page) {
+    return {
+      title: page.data.title,
+      description: page.data.description,
+      url: page.url,
+      id: page.url,
+      structuredData: page.data.structuredData,
+      tag: page.slugs[0],
+    }
+  },
+})
