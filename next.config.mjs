150b47e08846a2d88ee4e285ac2e27cfeefedb89
N0ctane._.Dev <n0ctaneDev@gmail.com>
2026-04-22 06:14:09 +0530

    Set basePath for the application

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   output: 'export',
+  basePath: '/fumadocs-editor',
   reactStrictMode: true,
   images: {
     remotePatterns: [
